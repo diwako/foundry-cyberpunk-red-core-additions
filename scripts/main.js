8a388e75313075ca8f5bfbdd665e3046e69cbf73
diwako <diwako@gmail.com>
2023-04-05 13:55:03 +0200

    Add deeper item piles integration

--- scripts/main.js
+++ scripts/main.js
@@ -225,18 +225,82 @@ Hooks.on("createChatMessage", async function (message) {
   "preRemoveItems",
   "preTransferAllItems",
 ].forEach((hookname) => {
-  Hooks.on(
-    `item-piles-${hookname}`,
-    function (actor, someBoolean1, itemObject, someBoolean2) {
-      if (itemObject.item.system.upgrades.length) {
-        ui.notifications.error(
-          "Dropping/trading/giving upgraded items will break parts of the character sheet. If you want to trade an upgraded item or drop it, tell the GM."
-        );
-        return false;
-      }
-      return true;
+  // Hooks.on(
+  //   `item-piles-${hookname}`,
+  //   function (actor, someBoolean1, itemObject, someBoolean2) {
+  //     if (itemObject.item.system.upgrades.length) {
+  //       ui.notifications.error(
+  //         "Dropping/trading/giving upgraded items will break parts of the character sheet. If you want to trade an upgraded item or drop it, tell the GM."
+  //       );
+  //       return false;
+  //     }
+  //     return true;
+  //   }
+  // );
+});
+
+async function handleItemPilesInteraction(source, itemData) {
+  // console.log({ source, itemData });
+  if (!game.user.isGM) return;
+  if (!Array.isArray(itemData)) {
+    itemData = [itemData];
+  }
+  if (source._actor) {
+    source = source._actor;
+  }
+
+  // if (!(await source.testUserPermission(game.user, "OWNER"))) return;
+  const itemsToDelete = [];
+  for (const itemInfo of itemData) {
+    // only process weapon item types with upgrades
+    if (
+      itemInfo?.quantity > 1 ||
+      itemInfo.item.type != "weapon" ||
+      !itemInfo.item.system.upgrades ||
+      !itemInfo.item.system.upgrades.length
+    )
+      continue;
+
+    for (const upgrade of itemInfo.item.system.upgrades) {
+      itemsToDelete.push(upgrade._id);
     }
-  );
+  }
+
+  if (itemsToDelete.length) {
+    setTimeout(
+      async function (source, itemsToDelete) {
+        // if (!(await source.testUserPermission(game.user, "OWNER"))) return;
+        const actualDelete = [];
+        for (const id of itemsToDelete) {
+          const item = await source.items.get(id);
+          if (item) actualDelete.push(id);
+        }
+
+        if (actualDelete.length) {
+          await source.deleteEmbeddedDocuments("Item", actualDelete);
+          if (
+            source.items.size == 0 &&
+            source?.flags["item-piles"]?.data?.deleteWhenEmpty &&
+            source.parent
+          )
+            await source.parent.delete();
+        }
+      },
+      5 * 1000,
+      source,
+      itemsToDelete
+    );
+  }
+}
+
+Hooks.on("item-piles-removeItems", function (target, itemDeltas) {
+  handleItemPilesInteraction(target, itemDeltas);
+});
+
+["dropItem", "transferItems", "giveItem"].forEach((hookname) => {
+  Hooks.on(`item-piles-${hookname}`, async function (source, _, itemData) {
+    handleItemPilesInteraction(source, itemData);
+  });
 });
 
 const api = {};
